6f641450e7603b9a6d7df0186df99ee47384fe70
candyolivia <candyoliviamawalim@gmail.com>
2016-03-29 20:42:55 +0700

    generate elliptical curve points

--- src/ecc/elgamal/KeyGenerator.java
+++ src/ecc/elgamal/KeyGenerator.java
@@ -5,10 +5,48 @@
  */
 package ecc.elgamal;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Candy Olivia Mawalim
  */
 public class KeyGenerator {
+    private ArrayList<Point> pointVect = new ArrayList<>();
+    
+    public void ellipticalCurveFunc(int a, int b, int p) {
+        int y; //y2 = x3 + ax + b
+        for (int x = 0; x < 15; x++) {
+            y = (int)Math.sqrt(x*x*x + a*x + b);
+            if (y*y == x*x*x + a*x + b) {
+                int xTemp = x;
+                if (xTemp > p || y > p) {
+                    xTemp %= p;
+                    y %= p;
+                }
+                
+                if (y != (p-y)) {
+                    Point temp = new Point(xTemp,(p-y));
+                    pointVect.add(temp);
+                }
+                
+                Point temp = new Point(xTemp,y);
+                pointVect.add(temp);
+            }
+        }
+    }
+    
+    public void printEllipticalPoints() {
+        for (int i = 0; i < pointVect.size(); i++) {
+            pointVect.get(i).print();
+        }
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        KeyGenerator kg = new KeyGenerator();
+        kg.ellipticalCurveFunc(2, 1, 5);
+        kg.printEllipticalPoints();
+    }
     
 }
---
+++ src/ecc/elgamal/Point.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecc.elgamal;
+
+/**
+ *
+ * @author Candy Olivia Mawalim
+ */
+public class Point {
+    private int x;
+    private int y;
+    
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+    
+    public Point(int _x, int _y) {
+        x = _x;
+        y = _y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    public void print() {
+        System.out.println("x = " + x + " y = " + y);
+    }
+}
